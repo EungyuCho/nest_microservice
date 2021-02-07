724519307a270da313358326dc044c632086364a
gyu <cho2304@naver.com>
2021-02-07 12:58:37 +0900

    :heavy_plus_sign: typeorm postgre @nestjs/config

---
+++ product/src/app.module.ts
@@ -0,0 +1,46 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import * as Joi from 'joi';
+import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env.dev',
+      validationSchema: Joi.object({
+        DB_HOST: Joi.string()
+          .valid()
+          .required(),
+        DB_PORT: Joi.string()
+          .valid()
+          .required(),
+        DB_USERNAME: Joi.string()
+          .valid()
+          .required(),
+        DB_PASSWORD: Joi.string()
+          .valid()
+          .required(),
+        DB_NAME: Joi.string()
+          .valid()
+          .required(),
+      }),
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      logging: true,
+      entities: [],
+      synchronize: true,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
